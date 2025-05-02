7750451358010ab17ac3eaf4585e3ca876246dff
0ncloud9 <seankangster.com>
2025-05-02 20:32:14 +0900

    vercel deploy --prod --prebuilt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/SubwayHero/",
+  base: process.env.VERCEL ? "/" : "/SubwayHero/",
   build: {
     sourcemap: true,
   },
